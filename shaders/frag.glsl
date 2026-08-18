0d004f0e60fc78def92a5c6283caea7556b39b0c
R0uT3r52 <nikita.m.gribanov@gmail.com>
2026-08-18 19:31:20 +0300

    Grid alpha channel interpolation added

--- shaders/frag.glsl
+++ shaders/frag.glsl
@@ -7,12 +7,22 @@ uniform float currentZoom;
 out vec4 fragCol;
 void main() {
     vec4 texCol = texture(tex, texCord);
-    if (currentZoom > 8.0) {
+
+    float minZoom = 13.0;
+    float maxZoom = 19.0;
+    float maxGridAlpha = 0.3;
+
+    if (currentZoom > minZoom) {
         vec2 pixCords = texCord * textureSize;
         vec2 gridDistance = min(fract(pixCords), 1.0 - fract(pixCords));
+
+        float zoomFactor = smoothstep(minZoom, maxZoom, currentZoom);
+
         float lineWidth = 1.0/currentZoom;
         if (gridDistance.x < lineWidth || gridDistance.y < lineWidth) {
-            vec4 gridCol = vec4(1.0, 1.0, 1.0, 0.3);
+            float alpha = maxGridAlpha * zoomFactor;
+
+            vec4 gridCol = vec4(1.0, 1.0, 1.0, alpha);
             fragCol = mix(texture(tex, texCord), gridCol, gridCol.a);
             return;
         }
